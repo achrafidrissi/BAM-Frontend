19c315aa51c7e324229051c93c012e6a7a2e9a15
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-07-23 20:52:04 +0100

    Resolving Errors

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,10 +6,11 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
